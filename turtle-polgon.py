3269bc516cbc2c2af0857e1d103ba2df71637e07
Usairim Kamal <usairimkamal2016@gmail.com>
2025-08-30 09:27:52 +0500

    Create turtle-polgon.py

---
+++ turtle-polgon.py
@@ -0,0 +1,15 @@
+import turtle
+turtle.Screen().bgcolor("orange")
+turtle.Screen().setup(300,400)
+polygon = turtle.Turtle()
+
+numsides = 6
+side_length = 70
+angle = 360.0 / numsides
+for i in range(numsides):
+  polygon.forward(side_length)
+  polygon.right(angle)
+
+
+
+turtle.done()
