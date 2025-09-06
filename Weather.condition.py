c3c1d2a8343ab47af14ac51ac004b1663365eaa2
Usairim Kamal <usairimkamal2016@gmail.com>
2025-09-06 09:47:30 +0500

    Create Weather.condition.py

---
+++ Weather.condition.py
@@ -0,0 +1,8 @@
+# Solution
+def weather_condition():
+    print('The weather is always in', spring)
+    print('The weather is seen in', autumn)
+
+spring = "autumn"
+autumn = spring
+weather_condition()
