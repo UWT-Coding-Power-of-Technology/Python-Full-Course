f0de2e4aca85a5c52ab2c42f41e305fe20ee0807
Usairim Kamal Azfar Wakeel <usairimkamal2016@gmail.com>
2025-11-08 09:39:43 +0500

    Create gk-quiz.py

---
+++ gk-quiz.py
@@ -0,0 +1,10 @@
+print("Welcome to the GK Quiz")
+print("What is Captial of UK?")
+print("1) Paris")
+print("2) London")
+print("3) Islamabad")
+choice = input("Enter 1, 2, or 3:")
+if choice == "2":
+    print("That's correct answer.")
+else:
+    print("Incorrect option.")
